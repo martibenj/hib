fcd6a45bd8294afa0c647614fb78f3c17082d362
polakov <spamwxc@gmail.com>
2015-08-18 14:18:00 +0200

    Ajout d'un builder pour la classe PersonneBnj

--- src/main/java/fr/test/hibernate/MainBnj.java
+++ src/main/java/fr/test/hibernate/MainBnj.java
@@ -2,6 +2,7 @@ package fr.test.hibernate;
 
 import org.hibernate.Session;
 
+import fr.test.hibernate.builder.PersonneBnjBuilder;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 import fr.test.hibernate.entities.PersonneBnj;
@@ -12,13 +13,15 @@ public class MainBnj
   public static void main(String[] args)
   {
     // Creating POJO
-    PodamFactory pojoFactory = new PodamFactoryImpl();
-    PersonneBnj user1 = pojoFactory.manufacturePojo(PersonneBnj.class);
-    user1.setPourcentageMochitude(0);
+    //PodamFactory pojoFactory = new PodamFactoryImpl();
+    //PersonneBnj user1 = pojoFactory.manufacturePojo(PersonneBnj.class);
+    PersonneBnj user1 = new PersonneBnjBuilder().setPourcentageMochitude(0).build();
+    //user1.setPourcentageMochitude(0);
 
-    PersonneBnj user2 = pojoFactory.manufacturePojo(PersonneBnj.class);
-    user2.setPrenom("le moche");
-    user2.setPourcentageMochitude(100);
+    // PersonneBnj user2 = pojoFactory.manufacturePojo(PersonneBnj.class);
+    PersonneBnj user2 = new PersonneBnjBuilder().setPrenom("le moche").setPourcentageMochitude(100).build();
+    //user2.setPrenom("le moche");
+    //user2.setPourcentageMochitude(100);
 
     // Persisting POJO
     Session session = HibernateUtil.getSessionFactory().openSession();
---
+++ src/main/java/fr/test/hibernate/builder/PersonneBnjBuilder.java
@@ -0,0 +1,57 @@
+// -----------------------------------------------------------------------------
+// Projet : test1
+// Client : Pôle Emploi
+// Auteur : Vincent Powazny / Bull S.A.S.
+// -----------------------------------------------------------------------------
+package fr.test.hibernate.builder;
+
+import fr.test.hibernate.entities.PersonneBnj;
+
+public class PersonneBnjBuilder
+{
+
+   private PersonneBnj personneBnj;
+
+   // ------------------------- Membres private -------------------------
+   // ------------------------- Constructeur -------------------------
+
+   public PersonneBnjBuilder()
+   {
+      personneBnj = new PersonneBnj();
+   }
+
+   public PersonneBnj build()
+   {
+      return personneBnj;
+   }
+
+
+
+   // ------------------------- Méthodes public -------------------------
+
+   public PersonneBnjBuilder setNom(String p_Nom)
+   {
+      personneBnj.setNom(p_Nom);
+      return this;
+   }
+
+   public PersonneBnjBuilder setPrenom(String p_Prenom)
+   {
+      personneBnj.setPrenom(p_Prenom);
+      return this;
+   }
+
+   public PersonneBnjBuilder setAge(int p_Age)
+   {
+      personneBnj.setAge(p_Age);
+      return this;
+   }
+
+   public PersonneBnjBuilder setPourcentageMochitude(int p_Pourcentage)
+   {
+      personneBnj.setPourcentageMochitude(p_Pourcentage);
+      return this;
+   }
+
+   // ------------------------- Méthodes private -------------------------
+}
