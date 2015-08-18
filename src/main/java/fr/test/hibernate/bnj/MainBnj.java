fe9ab6bb923763179b71947d18216f88263d3471
martiben <dfgd.dfgd@dfg.com>
2015-08-18 13:36:09 +0200

    Adding util class and first test with ids

---
+++ src/main/java/fr/test/hibernate/bnj/MainBnj.java
@@ -0,0 +1,31 @@
+package fr.test.hibernate.bnj;
+
+import org.hibernate.Session;
+
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+import fr.test.hibernate.bnj.entities.PersonneBnj;
+import fr.test.hibernate.bnj.persistence.HibernateUtil;
+
+public class MainBnj
+{
+  public static void main(String[] args)
+  {
+    // Creating POJO
+    PodamFactory pojoFactory = new PodamFactoryImpl();
+    PersonneBnj user1 = pojoFactory.manufacturePojo(PersonneBnj.class);
+    user1.setPourcentageMochitude(0);
+
+    PersonneBnj user2 = pojoFactory.manufacturePojo(PersonneBnj.class);
+    user2.setPrenom("le moche");
+    user2.setPourcentageMochitude(100);
+
+    // Persisting POJO
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    session.save(user1);
+    session.save(user2);
+    session.flush();
+
+    HibernateUtil.shutdown();
+  }
+}
